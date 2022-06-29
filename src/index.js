fcf6232465d8d7c4717295f42fac792059865839
Parkwochang <whwh3560@gmail.com>
2022-06-29 14:40:22 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./modules";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만든다
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<div></div>);
